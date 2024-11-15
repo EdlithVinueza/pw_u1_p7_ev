b5ad197f2ee2f9c19d2fd61e9a3efff953b6425b
EdlithVinueza <eavinuezaz1@uce.edu.ec>
2024-11-15 06:42:15 +0000

    Taller 6

---
+++ js/jsexterno.js
@@ -0,0 +1,14 @@
+function cambiarTexto(idelemento, valor) {
+    document.getElementById(idelemento).innerText = valor
+}
+
+function cambiarColor(idelemento, color) {
+    document.getElementById(idelemento).style.color = color;
+}
+
+function cambiarTamanio(idelemento, tamanio) {
+    document.getElementById(idelemento).style.fontSize = tamanio;
+}
+function agregarElementos(idelemento,elementohtml) {
+    document.getElementById(idelemento).innerHTML=document.getElementById(idelemento).innerHTML+elementohtml
+}
