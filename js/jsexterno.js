98473ac02ea21931f1a36f810ecc309eb4f67e7e
EdlithVinueza <eavinuezaz1@uce.edu.ec>
2024-11-15 07:28:30 +0000

    Tarea 4

--- js/jsexterno.js
+++ js/jsexterno.js
@@ -1,14 +1,47 @@
-function cambiarTexto(idelemento, valor) {
-    document.getElementById(idelemento).innerText = valor
+function cambiarColorLabel1(color) {
+    document.getElementById('label1').style.color = color;
 }
 
-function cambiarColor(idelemento, color) {
-    document.getElementById(idelemento).style.color = color;
+function cambiarTamanoLabel1(tamano) {
+    document.getElementById('label1').style.fontSize = tamano;
 }
 
-function cambiarTamanio(idelemento, tamanio) {
-    document.getElementById(idelemento).style.fontSize = tamanio;
+function cambiarTextoLabel1(texto) {
+    document.getElementById('label1').innerText = texto;
 }
-function agregarElementos(idelemento,elementohtml) {
-    document.getElementById(idelemento).innerHTML=document.getElementById(idelemento).innerHTML+elementohtml
+
+function agregarElemento(texto) {
+    document.getElementById('idmenu').innerHTML += '<li><a href="#" style="color: black;">' + texto + '</a></li>';
+}
+
+function cambiarFondoInput1(color) {
+    document.getElementById('numero1').style.backgroundColor = color;
+}
+
+function cambiarFondoInput2(color) {
+    document.getElementById('numero2').style.backgroundColor = color;
+}
+
+function cambiarPesoTextoLabel2(peso) {
+    document.getElementById('label2').style.fontWeight = peso;
+}
+
+function cambiarEstiloTextoLabel2(estilo) {
+    document.getElementById('label2').style.fontStyle = estilo;
+}
+
+function cambiarColorCalculadora(color) {
+    document.getElementById('calculadora').style.color = color;
+}
+
+function cambiarTamanoCalculadora(tamano) {
+    document.getElementById('calculadora').style.fontSize = tamano;
+}
+
+function cambiarColorH2(color) {
+    document.getElementById('ingresaNumeros').style.color = color;
+}
+
+function cambiarTamanoH2(tamano) {
+    document.getElementById('ingresaNumeros').style.fontSize = tamano;
 }
